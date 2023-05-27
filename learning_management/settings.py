96c9eda70a4a9d98233c1aaccb487a0c8d361da5
viktor <onik.office@gmail.com>
2023-05-27 15:42:09 +0300

    Add AUTHENTICATION

--- learning_management/settings.py
+++ learning_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'lm',
 ]
 
@@ -71,8 +72,13 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 WSGI_APPLICATION = 'learning_management.wsgi.application'
